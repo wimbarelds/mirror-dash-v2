c8c1c2aac7b10f1270e01da0fdf9c0330758532a
wimbarelds <wim.barelds@gmail.com>
2024-05-19 13:15:43 +0200

    add base dir to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import process from 'process';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
+    base: env.BASE_DIR || '/',
   };
 });
