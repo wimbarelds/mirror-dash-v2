eca38c9f2b724680d10e8fa8e8e28fe1ba4ee991
wimbarelds <wim.barelds@gmail.com>
2026-05-01 15:27:52 +0200

    Update fetch URL to use CORS proxy

--- src/util/ov/fetch-ov.ts
+++ src/util/ov/fetch-ov.ts
@@ -2,7 +2,10 @@ import { OVDataSchema } from './schema';
 
 export function getOVData() {
   const halteId = import.meta.env.VITE_OV_HALTE_ID;
-  return fetch(`//v0.ovapi.nl/tpc/${halteId}`)
+  const targetUrl = `http://v0.ovapi.nl/tpc/${halteId}`
+  const proxyUrl = `https://corsproxy.io/?${targetUrl}`
+
+  return fetch(proxyUrl)
     .then((response) => response.json())
     .then((data) => OVDataSchema.parse(data))
     .then((data) => data[halteId]);
